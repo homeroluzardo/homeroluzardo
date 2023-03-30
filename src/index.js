05b6ded3165c062be2f1d86f174da6987876b8ae
Homero Luzardo <homeroeluzardo@gmail.com>
2023-03-29 20:54:43 -0600

    Update react-dom/client

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import ReactDOM from "react-dom";
 import "../src/styles/Index.css";
@@ -5,5 +6,21 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+serviceWorker.unregister();
+*/
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "../src/styles/Index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 serviceWorker.unregister();
