f12944a664e1ce5d0494b01b8a0ed100fdb99890
Homero Luzardo <homeroeluzardo@gmail.com>
2023-03-29 21:08:19 -0600

    Update main image

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
-/*
-import React from "react";
-import ReactDOM from "react-dom";
-import "../src/styles/Index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
-
-ReactDOM.render(<App />, document.getElementById("root"));
-
-serviceWorker.unregister();
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "../src/styles/Index.css";
