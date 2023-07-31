0c8555103c67ea61a9ee4132e111e7f726c213b9
Homero Luzardo <homeroeluzardo@gmail.com>
2023-07-31 16:59:17 -0600

    Skills Update

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -33,6 +33,10 @@ function Skills() {
         <h2 className="subtitle">Skills</h2>
 
         <blockquote className="blockquote text-white">
+          <p className="mb-0 font-italic">Click on each image for details.</p>
+        </blockquote>
+
+        <blockquote className="blockquote text-white">
           <p className="mb-0">
             HTML 5, CSS 3, JavaScript, React, Node JS, JQuery, Npm, Bootstrap,
             Sass, Git, Github.
@@ -46,57 +50,85 @@ function Skills() {
           <div>
             <div>
               <img src={html5} alt="logo html" />
-              <span className="tooltip">HTML</span>
+              <span className="tooltip">
+                HTML is the standard markup language for Web pages.
+              </span>
             </div>
 
             <div>
               <img src={css3} alt="logo css" />
-              <span className="tooltip">CSS</span>
+              <span className="tooltip">
+                CSS is the language we use to style an HTML document.
+              </span>
             </div>
 
             <div>
               <img src={imgjavascript} alt="logo javascript" />
-              <span className="tooltip">JavaScript</span>
+              <span className="tooltip">
+                JavaScript is a programming language that adds interactivity to
+                your website.
+              </span>
             </div>
 
             <div>
               <img src={imgreact} alt="logo react" />
-              <span className="tooltip">React</span>
+              <span className="tooltip">
+                React is an open-source JavaScript framework and library
+                developed by Facebook.
+              </span>
             </div>
 
             <div>
               <img src={nodejs} alt="logo nodejs" />
-              <span className="tooltip">Node JS</span>
+              <span className="tooltip">
+                Node JS is an open-source, cross-platform JavaScript runtime
+                environment.
+              </span>
             </div>
 
             <div>
               <img src={imgjquery} alt="logo jquery" />
-              <span className="tooltip">JQuery</span>
+              <span className="tooltip">
+                jQuery is a fast, small, and feature-rich JavaScript library.
+              </span>
             </div>
 
             <div>
               <img src={npm} alt="logo npm" />
-              <span className="tooltip">NPM</span>
+              <span className="tooltip">
+                NPM is the world's largest Software Library (Registry).
+              </span>
             </div>
 
             <div>
               <img src={bootstrap} alt="logo bootstrap" />
-              <span className="tooltip">Bootstrap</span>
+              <span className="tooltip">
+                Bootstrap is a powerful, feature-packed frontend toolkit.
+              </span>
             </div>
 
             <div>
               <img src={sass} alt="logo sass" />
-              <span className="tooltip">Sass</span>
+              <span className="tooltip">
+                SASS is a popular CSS pre-processor which can be used to style
+                HTML elements.
+              </span>
             </div>
 
             <div>
               <img src={git} alt="logo git" />
-              <span className="tooltip">Git</span>
+              <span className="tooltip">
+                Git is a free and open source distributed version control
+                system.
+              </span>
             </div>
 
             <div>
               <img src={github} alt="logo github" />
-              <span className="tooltip">GitHub</span>
+              <span className="tooltip">
+                GitHub is a code hosting platform for version control and
+                collaboration.
+              </span>
             </div>
           </div>
         </div>
@@ -114,22 +146,33 @@ function Skills() {
           <div>
             <div>
               <img src={clickfunnels} alt="logo clickfunnels" />
-              <span className="tooltip">ClickFunnels</span>
+              <span className="tooltip">
+                ClickFunnels is an online tool for building high-converting
+                websites.
+              </span>
             </div>
 
             <div>
               <img src={unbounce} alt="logo unbounce" />
-              <span className="tooltip">Unbounce</span>
+              <span className="tooltip">
+                Unbounce is a AI-powered landing page builder.
+              </span>
             </div>
 
             <div>
               <img src={leadpages} alt="logo leadpages" />
-              <span className="tooltip">Leadpages</span>
+              <span className="tooltip">
+                Leadpages is an online tool that you can use to collect email
+                subscribers.
+              </span>
             </div>
 
             <div>
               <img src={kajabi} alt="logo kajabi" />
-              <span className="tooltip">Kajabi</span>
+              <span className="tooltip">
+                Kajabi is the leading all-in-one platform for creator-led
+                businesses.
+              </span>
             </div>
           </div>
         </div>
@@ -145,12 +188,18 @@ function Skills() {
           <div>
             <div>
               <img src={wordpress} alt="logo wordpress" />
-              <span className="tooltip">Wordpress</span>
+              <span className="tooltip">
+                Wordpress is the simplest, most popular way to create your own
+                website or blog.
+              </span>
             </div>
 
             <div>
               <img src={elementor} alt="logo elementor" />
-              <span className="tooltip">Elementor</span>
+              <span className="tooltip">
+                Elementor is the leading website builder platform for
+                professionals on WordPress.
+              </span>
             </div>
           </div>
         </div>
@@ -168,22 +217,33 @@ function Skills() {
           <div>
             <div>
               <img src={hubspot} alt="logo hubspot" />
-              <span className="tooltip">Hubspot</span>
+              <span className="tooltip">
+                Hubspot helps you to increase your leads, accelerate sales, or
+                simplify your process.
+              </span>
             </div>
 
             <div>
               <img src={activecampaign} alt="logo activecampaign" />
-              <span className="tooltip">ActiveCampaign</span>
+              <span className="tooltip">
+                ActiveCampaign is an automation tool based on email marketing.
+              </span>
             </div>
 
             <div>
               <img src={keap} alt="logo keap" />
-              <span className="tooltip">Keap</span>
+              <span className="tooltip">
+                Keap offers an e-mail marketing and sales platform for small
+                businesses.
+              </span>
             </div>
 
             <div>
               <img src={mailchimp} alt="logo mailchimp" />
-              <span className="tooltip">Mailchimp</span>
+              <span className="tooltip">
+                Mailchimp is an email marketing platform that helps you reach
+                your audience.
+              </span>
             </div>
           </div>
         </div>
@@ -199,17 +259,25 @@ function Skills() {
           <div>
             <div>
               <img src={finalcut} alt="logo final cut pro" />
-              <span className="tooltip">Final Cut Pro</span>
+              <span className="tooltip">
+                Final Cut Pro is an app for creating, editing, and producing
+                videos.
+              </span>
             </div>
 
             <div>
               <img src={screenflow} alt="logo screenflow" />
-              <span className="tooltip">Screenflow</span>
+              <span className="tooltip">
+                Screenflow is a screencasting and video editing software for
+                macOS.
+              </span>
             </div>
 
             <div>
               <img src={imovie} alt="logo imovie" />
-              <span className="tooltip">iMovie</span>
+              <span className="tooltip">
+                iMovie is a free video-editing program for Apple users.
+              </span>
             </div>
           </div>
         </div>
